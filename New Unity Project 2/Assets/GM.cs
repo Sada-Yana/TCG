efa61d4639bc117d346bcc6411e649703ff5095d
Sada-Yana <ggag1121@yahoo.co.jp>
2017-05-29 03:42:39 +0900

    AI改修

--- New Unity Project 2/Assets/GM.cs
+++ New Unity Project 2/Assets/GM.cs
@@ -13,8 +13,11 @@ public class GM : MonoBehaviour {
 
     int i = 0;
 
+    public static int my_win = 0;
+    public static int your_win = 0;
 
-   [SerializeField] int deck_count;
+
+    [SerializeField] int deck_count;
     public Image[] ima;
 
     public Text mine_num;
@@ -26,11 +29,18 @@ public class GM : MonoBehaviour {
     public Text your_point;
 
     public Text result;
+    public Text count;
 
     public int mp = 0;
     public int yp = 0;
+    private int j = 0;
 
     [SerializeField] bool player_play = true;
+    [SerializeField] bool Player_AI = false;
+
+    [SerializeField] bool result_check = false;
+
+
 	// Use this for initialization
 	void Start () {
 
@@ -56,15 +66,22 @@ public class GM : MonoBehaviour {
         if (tcg_.Count > 0 && !(tcg_.Count == 1 && (mines.Count == 1 || yours.Count == 1)))
         {
             if (!player_play)
-                AI();
-            else
+                AI_2();
+            else if (!Player_AI && player_play)
                 Use_Card();
+            else if (Player_AI && player_play)
+                AI_m();
+
+            
         }
 
         if(tcg_.Count <= 1)
         {
             Result();
         }
+
+
+
     }
 
 
@@ -73,20 +90,35 @@ public class GM : MonoBehaviour {
     /// </summary>
     void Result()
     {
-        if(mp > yp)
+        if (j == 0)
         {
-            result.text = "貴方の勝ち";
+            if (mp > yp)
+            {
+                result.text = "貴方の勝ち";
+                my_win++;
+            }
+            else if (yp > mp)
+            {
+                result.text = "貴方の負け";
+                your_win++;
+            }
+            else
+            {
+                result.text = "引き分け";
+            }
+            j++;
         }
-         else if(yp > mp)
+
+        if(Player_AI)
         {
-            result.text = "貴方の負け";
-        }
-        else
-        {
-            result.text = "引き分け";
-        }
-        if(Input.GetKeyDown(KeyCode.Space))
             SceneManager.LoadScene(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+            j = 0;
+        }
+
     }
 
     /// 
@@ -197,6 +229,8 @@ public class GM : MonoBehaviour {
         {
             card.text += i + ": " + Substitution(mines[i].effect) + " " + mines[i].point + "\n";
         }
+
+        count.text = "貴方の勝数" + my_win + "\n相手の勝数" + your_win;
 
     }
 
@@ -395,7 +429,9 @@ public class GM : MonoBehaviour {
     }
 
 
-
+    /// <summary>
+    /// 従来型AI
+    /// </summary>
     void AI()
     {
 
@@ -434,6 +470,175 @@ public class GM : MonoBehaviour {
         Card_Effected(yours[eff]);
         yours.RemoveAt(eff);
         Debug.Log(eff);
+
+    }
+    /// <summary>
+    /// 自分側AI
+    /// </summary>
+    void AI_m()
+    {
+
+        int tmp_add = -1;
+        int tmp_draw = -1;
+        int tmp_pen = -1;
+        int tmp_spe = -1;
+
+        int eff = 0;
+        int i = 0;
+
+        for (i = 0; i < mines.Count; i++)
+        {
+            if (mines[i].effect == 2)
+                tmp_add = i;
+            if (mines[i].effect == 3)
+                tmp_pen = i;
+            if (mines[i].effect == 1)
+                tmp_draw = i;
+            if (mines[i].effect >= 4)
+                tmp_spe = i;
+        }
+
+
+        if (tmp_spe >= 0)
+        {
+            eff = tmp_spe;
+        }
+        else if (tmp_add >= 0)
+            eff = tmp_add;
+        else if (tmp_draw >= 0)
+            eff = tmp_draw;
+        else if (tmp_pen >= 0)
+            eff = tmp_pen;
+
+        Card_Effected(mines[eff]);
+        mines.RemoveAt(eff);
+        //Debug.Log(eff);
+
+    }
+
+    /// <summary>
+    /// 改良版AI
+    /// </summary>
+    void AI_2()
+    {
+        /*
+        int tmp_add = -1;
+        int tmp_draw = -1;
+        int tmp_pen = -1;
+        int tmp_spe = -1;
+        */
+
+        List<TCG.Param> add = new List<TCG.Param>();
+        List<TCG.Param> draw = new List<TCG.Param>();
+        List<TCG.Param> pen = new List<TCG.Param>();
+        List<TCG.Param> spe = new List<TCG.Param>();
+
+        int eff = 0;
+        int i = 0;
+
+        //仮置き
+        for (i = 0; i < yours.Count; i++)
+        {
+            if (yours[i].effect == 1)
+            {
+                draw.Add(yours[i]);
+            }
+
+            if (yours[i].effect == 2)
+            {
+                add.Add(yours[i]);
+            }
+
+            if (yours[i].effect == 3)
+            {
+                pen.Add(yours[i]);
+            }
+
+            if (yours[i].effect >= 4)
+            {
+                spe.Add(yours[i]);
+            }
+        }
+
+        //ソート
+        draw.Sort((a, b) => b.point - a.point);
+        add.Sort((a, b) => b.point - a.point);
+        pen.Sort((a, b) => b.point - a.point);
+        spe.Sort((a, b) => b.point - a.point);
+
+
+        if (spe.Count > 0)
+        {
+            eff = yours.FindIndex(s => s.id == spe[0].id);
+            goto Finish;
+        }
+
+        else if(yours.Count < 4 && draw.Count > 0)
+        {
+            eff = yours.FindIndex(d => d.id == draw[0].id);
+            goto Finish;
+        }
+        else
+        {
+            //addかpenがあるなら
+
+            if ((add.Count > 0 || pen.Count > 0 ) )
+            {
+                if (add.Count > 0 && pen.Count > 0)
+                {
+                    if ((add[0].point > pen[0].point) )
+                    {
+                        eff = yours.FindIndex(d => d.id == add[0].id);
+                        goto Finish;
+                    }
+                    else if ((add[0].point < pen[0].point) )
+                    {
+                        eff = yours.FindIndex(d => d.id == pen[0].id);
+                        goto Finish;
+                    }
+                    else 
+                    {
+                        eff = yours.FindIndex(d => d.id == add[0].id);
+                        goto Finish;
+                    }
+                }
+                else if (add.Count > 0)
+                {
+                    eff = yours.FindIndex(d => d.id == add[0].id);
+                    goto Finish;
+                }
+                else if (pen.Count > 0)
+                {
+                    eff = yours.FindIndex(d => d.id == pen[0].id);
+                    goto Finish;
+                }
+            }
+
+            else if(draw.Count > 0)
+            {
+                eff = yours.FindIndex(d => d.id == draw[0].id);
+                goto Finish;
+            }
+
+            else
+                goto Else_Finish;
+
+        }
+
+
+    Else_Finish:
+        {
+            Debug.Log("何もできない");
+            Card_Draw(1);
+            player_play = !player_play;
+        }
+    Finish:
+        {
+            Card_Effected(yours[eff]);
+            yours.RemoveAt(eff);
+           // Debug.Log(eff);
+        }
+
 
     }
 }
